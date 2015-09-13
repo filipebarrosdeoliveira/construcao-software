5d47cde3afb264dff1219f0ec4dfd4df1887e76d
filipebarrosdeoliveira <barros7@icloud.com>
2015-09-13 03:49:35 -0300

    Produto

---
+++ Produto/src/main/java/br/ufg/inf/es/construcao/produto/Produto.java
@@ -0,0 +1,7 @@
+package br.ufg.inf.es.construcao.produto;
+
+/**
+ * Created by filipebarros on 9/13/15.
+ */
+public class Produto {
+}
---
+++ Produto/src/test/java/br/ufg/inf/es/construcao/produto/ProdutoTest.java
@@ -0,0 +1,14 @@
+package br.ufg.inf.es.construcao.produto;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by filipebarros on 9/13/15.
+ */
+public class ProdutoTest {
+
+    @org.junit.Test
+    public void testProduto() throws Exception {
+
+    }
+}
