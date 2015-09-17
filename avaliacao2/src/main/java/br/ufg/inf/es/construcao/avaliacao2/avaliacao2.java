a39014052518375abdb50580f23ac6ca9568fead
filipebarrosdeoliveira <barros7@icloud.com>
2015-09-17 20:03:32 -0300

    avaliacao2

---
+++ avaliacao2/src/main/java/br/ufg/inf/es/construcao/avaliacao2/avaliacao2.java
@@ -0,0 +1,7 @@
+package br.ufg.inf.es.construcao.avaliacao2;
+
+/**
+ * Created by filipebarros on 9/17/15.
+ */
+public class avaliacao2 {
+}
---
+++ avaliacao2/src/test/java/br/ufg/inf/es/construcao/avaliacao2/avaliacao2Test.java
@@ -0,0 +1,14 @@
+package br.ufg.inf.es.construcao.avaliacao2;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by filipebarros on 9/17/15.
+ */
+public class avaliacao2Test {
+
+    @org.junit.Test
+    public void testPrimo() throws Exception {
+
+    }
+}
