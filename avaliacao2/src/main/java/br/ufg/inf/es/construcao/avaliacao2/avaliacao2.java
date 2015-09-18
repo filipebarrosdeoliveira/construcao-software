4184bdbb76dfde38b7be3066f835a21cf6a134eb
filipebarrosdeoliveira <barros7@icloud.com>
2015-09-17 21:16:13 -0300

    test1

--- avaliacao2/src/main/java/br/ufg/inf/es/construcao/avaliacao2/avaliacao2.java
+++ avaliacao2/src/main/java/br/ufg/inf/es/construcao/avaliacao2/avaliacao2.java
@@ -1,7 +1,39 @@
 package br.ufg.inf.es.construcao.avaliacao2;
 
 /**
- * Created by filipebarros on 9/17/15.
+ * Avaliação 2.
+ * Aluno: Filipe Barros de Oliveira.
+ * Construção de Software.
  */
+
+
 public class avaliacao2 {
+
+    /**
+     * @param n um valor inteiro.
+     * @return true se o valor fornecido for primo e false caso contrário.
+     */
+
+    public static boolean primo(int n) {
+
+        int i = 2;
+
+        if (n < 1) {
+            throw new IllegalArgumentException("n");
+        }
+
+        if (n % i == 0) {
+            return false;
+        }
+
+
+        while (i <= n) {
+            i = i + 1;
+        }
+
+        return true;
+    }
 }
+
+
+
--- avaliacao2/src/test/java/br/ufg/inf/es/construcao/avaliacao2/avaliacao2Test.java
+++ avaliacao2/src/test/java/br/ufg/inf/es/construcao/avaliacao2/avaliacao2Test.java
@@ -1,16 +1,24 @@
 package br.ufg.inf.es.construcao.avaliacao2;
 
+import org.junit.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
 
-/**
- * Created by filipebarros on 9/17/15.
- */
 public class avaliacao2Test {
 
     @Test
     public void testPrimo() throws Exception {
+
+        Assert.assertTrue(avaliacao2.primo(7));
+
+    }
+
+
+    @Test
+    public void TestPrimo() throws Exception {
+
+
+        Assert.assertFalse(avaliacao2.primo(288));
 
     }
 }
