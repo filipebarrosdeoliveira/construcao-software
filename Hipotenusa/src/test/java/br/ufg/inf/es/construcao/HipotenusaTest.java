d7237fccd970116cd229e2f029b951c33a78840b
filipebarrosdeoliveira <barros7@icloud.com>
2015-09-24 20:11:08 -0300

    Hipotenusa

---
+++ Hipotenusa/src/test/java/br/ufg/inf/es/construcao/HipotenusaTest.java
@@ -0,0 +1,16 @@
+package br.ufg.inf.es.construcao;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by filipebarros on 9/24/15.
+ */
+public class HipotenusaTest {
+
+    @Test
+    public void testHipotenusa() throws Exception {
+
+    }
+}
