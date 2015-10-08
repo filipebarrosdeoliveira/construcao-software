e60313150920cceee825f80228f2d58566110f40
filipebarrosdeoliveira <barros7@icloud.com>
2015-10-08 18:58:20 -0300

    Produto

--- Produto/src/test/java/br/ufg/inf/es/construcao/produto/ProdutoTest.java
+++ Produto/src/test/java/br/ufg/inf/es/construcao/produto/ProdutoTest.java
@@ -1,32 +1,16 @@
 package br.ufg.inf.es.construcao.produto;
 
-import org.junit.Assert;
 import org.junit.Test;
 
+import static org.junit.Assert.*;
+
+/**
+ * Created by filipebarros on 10/8/15.
+ */
 public class ProdutoTest {
 
-    @Test(expected = IllegalArgumentException.class)
-    public void testPrimeiroParametroInvalido() throws Exception {
-        Produto.produto(-1, 10);
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void testsegundoParametroInvalido() throws Exception {
-        Produto.produto(1, -10);
-    }
-
     @Test
-    public void testCasosDeFronteira() {
-        Assert.assertEquals(0, Produto.produto(0, 1));
-        Assert.assertEquals(0, Produto.produto(1, 0));
-        Assert.assertEquals(10, Produto.produto(1, 10));
-        Assert.assertEquals(10, Produto.produto(10, 1));
-    }
+    public void testProduto() throws Exception {
 
-    @Test
-    public void testCasoTrivial() {
-        Assert.assertEquals(100, Produto.produto(10, 10));
-        Assert.assertEquals(1000, Produto.produto(10, 100));
-        Assert.assertEquals(987 * 19, Produto.produto(987, 19));
     }
 }
