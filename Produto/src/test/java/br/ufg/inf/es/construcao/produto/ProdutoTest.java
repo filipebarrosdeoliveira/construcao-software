e1629c1a5dc8fa9e75ca45c04c59ecbb946b5795
filipebarrosdeoliveira <barros7@icloud.com>
2015-10-08 19:04:27 -0300

    adicionar

--- Produto/src/test/java/br/ufg/inf/es/construcao/produto/ProdutoTest.java
+++ Produto/src/test/java/br/ufg/inf/es/construcao/produto/ProdutoTest.java
@@ -1,16 +1,21 @@
 package br.ufg.inf.es.construcao.produto;
 
-import org.junit.Test;
+import org.junit.*;
 
-import static org.junit.Assert.*;
 
-/**
- * Created by filipebarros on 10/8/15.
- */
 public class ProdutoTest {
 
-    @Test
-    public void testProduto() throws Exception {
 
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testPrimeiroParametroInvalido() throws Exception {
+        Produto.produto(-1, 10);
     }
+
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testsegundoParametroInvalido() throws Exception {
+        Produto.produto(1, -10);
+    }
+
 }
