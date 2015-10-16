1492600bea393c76010e8a8130497e37302ae273
filipebarrosdeoliveira <barros7@icloud.com>
2015-10-15 21:51:43 -0300

    acrescentar

---
+++ Primos/src/main/java/br/ufg/inf/es/construcao/Primos.java
@@ -0,0 +1,7 @@
+package br.ufg.inf.es.construcao;
+
+/**
+ * Created by filipebarros on 10/15/15.
+ */
+public class Primos {
+}
---
+++ Razao-Aurea/src/test/java/br/ufg/inf/es/construcao/RazaoAureaTest.java
@@ -0,0 +1,14 @@
+package br.ufg.inf.es.construcao;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by filipebarros on 10/15/15.
+ */
+public class RazaoAureaTest {
+
+    @org.junit.Test
+    public void testRazaoAurea() throws Exception {
+
+    }
+}
