eb2a842c9f7465419481154659415893fef7a724
filipebarrosdeoliveira <barros7@icloud.com>
2015-10-29 20:38:03 -0200

    acrescentar

--- Razao-Aurea/src/main/java/br/ufg/inf/es/construcao/RazaoAurea.java
+++ Razao-Aurea/src/main/java/br/ufg/inf/es/construcao/RazaoAurea.java
@@ -1,7 +1,32 @@
 package br.ufg.inf.es.construcao;
 
-/**
- * Created by filipebarros on 10/15/15.
- */
+
+
 public class RazaoAurea {
+
+    public static double razaoAurea(int t1, int t2, int fator) {
+        if (t1 <= 0) {
+            throw new IllegalArgumentException("t1");
+        }
+
+        if (t2 <= t1) {
+            throw new IllegalArgumentException("t2 não é maior que t1");
+        }
+
+        if (fator <= 0) {
+            throw new IllegalArgumentException("Fator");
+        }
+
+        double tc = t2;
+        double ta = t1;
+        int i = 1;
+
+        while (i <= fator) {
+            tc = tc + ta;
+            ta = tc - ta;
+            i = i + 1;
+        }
+
+        return tc / ta;
+    }
 }
--- Razao-Aurea/src/test/java/br/ufg/inf/es/construcao/RazaoAureaTest.java
+++ Razao-Aurea/src/test/java/br/ufg/inf/es/construcao/RazaoAureaTest.java
@@ -1,14 +1,29 @@
 package br.ufg.inf.es.construcao;
 
-import static org.junit.Assert.*;
+import org.junit.*;
 
-/**
- * Created by filipebarros on 10/15/15.
- */
+
 public class RazaoAureaTest {
 
-    @org.junit.Test
+    @Test (expected = IllegalArgumentException.class)
+    public void testt1Invalido() throws Exception {
+        RazaoAurea.razaoAurea(0,7,9);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testt2Invalido() throws  Exception{
+        RazaoAurea.razaoAurea(1,-1,2);
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void testfatorInvalido() throws Exception {
+        RazaoAurea.razaoAurea(1,1,-1);
+    }
+
+    @Test
     public void testRazaoAurea() throws Exception {
+        Assert.assertEquals(1.61803399d, RazaoAurea.razaoAurea(2,6,1000), 0.00001d);
 
     }
+
 }
